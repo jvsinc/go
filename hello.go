2ed4ce41c775d2dd62aba90ee8ecf8b294a7a3f2
John Dondapati <djohnjimmy@gmail.com>
2015-03-09 01:18:55 -0400

    updated hello.g

--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/djohnjimmy/stringutil"
+)
 
 func main() {
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
     fmt.Printf("hello, world\n")
 }
